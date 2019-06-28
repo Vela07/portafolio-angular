b430b5a7bc5fe1df5a0296ee59eda001f7dfb9bb
Alfredo Vela <tonyvelamtz@hotmail.com>
2019-06-28 12:29:11 -0500

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,36 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
 
     console.log('Servicio de InfoPagina');
+    this.cargarInfo();
+    this.cargarEquipo();
+   
+  }
 
-    //Leer el archivo JSON
+  private cargarInfo(){
+      //Leer el archivo JSON
 
-    this.http.get('assets/data/data-pagina.json')
+      this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-         console.log(resp);
       });
   }
+
+  private cargarEquipo(){
+      //Leer el archivo JSON
+
+      this.http.get('https://angular-template-71fd5.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo = resp;
+         // console.log(resp);
+      });
+  }
+
 }
